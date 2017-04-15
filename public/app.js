e972d66242cc3e8596d0b47e125c42171345e9aa
Colin Farquhar <cifarquhar@gmail.com>
2017-04-15 16:50:30 +0100

    refactor html and currency view

--- public/app.js
+++ public/app.js
@@ -1,22 +1,31 @@
 var app = function(){
 
   var initialCurrency = new Currency("GBP")
-  var targetCurrency = new Currency("USD")
+  // var targetCurrency = new Currency("USD")
   var initialCurrencyView = new CurrencyView(document.querySelector("#input-currency"))
-  var targetCurrencyView = new CurrencyView(document.querySelector("#target-currency"))
+  // var targetCurrencyView = new CurrencyView(document.querySelector("#target-currency"))
 
 
   initialCurrency.getData(function(currency){
     console.log(currency)
-  initialCurrencyView.render(currency)
-  initialCurrencyView.createOption(currency)
+    initialCurrencyView.render(currency)
+    // initialCurrencyView.getObject(currency)
+    initialCurrencyView.createTargetOption(currency)
+    initialCurrencyView.selectElement.addEventListener("change",function(){
+    initialCurrencyView.updateView(currency,this.value)
+    })
+   
   })
+    // console.log(initialCurrency)
+    
+  // targetCurrency.getData(function(currency){
+  //   console.log(currency)
+  //   targetCurrencyView.render(currency)
+  //   // targetCurrencyView.createOption(currency)
+  // })
 
-  targetCurrency.getData(function(currency){
-    console.log(currency)
-  targetCurrencyView.render(currency)
-  targetCurrencyView.createOption(currency)
-  })
+  // var exchangeView = new ExchangeView(document.querySelector("#rate"),initialCurrency,targetCurrency)
+  // exchangeView.render()
 
 
 }
--- public/currency_view.js
+++ public/currency_view.js
@@ -4,24 +4,64 @@ var CurrencyView = function(divElement){
 
 CurrencyView.prototype = {
 
+  getObject: function(currency){
+    this.currency = currency
+  },
+
   render: function(currency){
-    // console.log(this)
-    var currencyStatement = document.createElement("p")
+    var currencyStatement = document.querySelector("#input-code")
+    var exchangeStatement = document.querySelector("#input-rate")
     var currencyCode = currency.base
-    currencyStatement.innerText = currencyCode
-    this.divElement.appendChild(currencyStatement)
+    var exchangeCodes = Object.keys(currency.rates)
+    var exchangeRates = Object.values(currency.rates)
+    var defaultExchangeCode = exchangeCodes[0]
+    var defaultExchangeRate = exchangeRates[0]
+    currencyStatement.innerText = "Currency: " + currencyCode
+    exchangeStatement.innerText = "Exchange Rate (" + defaultExchangeCode + "): " + defaultExchangeRate
+    // this.divElement.appendChild(currencyStatement)
+    // this.divElement.appendChild(exchangeStatement)
     },
   
     createOption: function(currency){
       var selectElement = document.createElement("select")
-      var otherCurrencies = Object.keys(currency.rates)
-      console.log(otherCurrencies)
-      otherCurrencies.forEach(function(currency){
+      // var otherCurrencies = Object.keys(currency.rates)
+      // this.otherCurrencies = otherCurrencies
+      // console.log(this.otherCurrencies)
+      otherCurrencies.forEach(function(currency,index){
         var optionElement = document.createElement("option")
+        optionElement.value = index
         optionElement.text = currency
         selectElement.appendChild(optionElement)
       })
       this.divElement.appendChild(selectElement)
+      this.selectElement = selectElement
+      // console.log(this.selectElement)
+    },
+    createTargetOption: function(currency){
+      var selectElement = document.createElement("select")
+      var otherCurrencies = Object.keys(currency.rates)
+      // this.otherCurrencies = otherCurrencies
+      // console.log(this.otherCurrencies)
+      otherCurrencies.forEach(function(currency,index){
+        var optionElement = document.createElement("option")
+        optionElement.value = index
+        optionElement.text = currency
+        selectElement.appendChild(optionElement)
+      })
+      this.divElement.appendChild(selectElement)
+      this.selectElement = selectElement
+      console.log(currency.rates)
+    },
+    updateView: function(currency,index){
+      var currencyStatement = document.querySelector("#input-code")
+      var exchangeStatement = document.querySelector("#input-rate")
+      var currencyCode = currency.base
+      var exchangeCodes = Object.keys(currency.rates)
+      var exchangeRates = Object.values(currency.rates)
+      var exchangeCode = exchangeCodes[index]
+      var exchangeRate = exchangeRates[index]
+      currencyStatement.innerText = "Currency: " + currencyCode
+      exchangeStatement.innerText = "Exchange Rate (" + exchangeCode + "): " + exchangeRate
     }
 
 }
