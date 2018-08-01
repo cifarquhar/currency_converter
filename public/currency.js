e7f2065ab5f2b2161119c02c091e47ff25c908fd
cifarquhar <colin.farquhar@codeclan.com>
2018-08-01 13:16:00 +0100

    refactor api request

--- public/currency.js
+++ public/currency.js
@@ -5,12 +5,15 @@ var Currency = function(currency){
 Currency.prototype = {
 
   getData: function(callback){
+    var key = "88c0871b1c32feaf2082dc168d45b14e"
     var request = new XMLHttpRequest()
-    request.open("GET","http://api.fixer.io/latest?base="+this.currency)
+    request.open("GET","http://data.fixer.io/api/latest?access_key="+key) //?base="+this.currency)
     request.onload = function(){
       if(request.status === 200){
         var jsonString = request.responseText
+        var oldCode = this.currency
         this.currency = JSON.parse(jsonString)
+        console.log(this.currency.rates[oldCode])
         callback(this.currency)
       }
     }.bind(this)
