b46ec2109a0551bafe6130ec6254ff9291b53545
cifarquhar <colin.farquhar@codeclan.com>
2018-08-01 15:50:29 +0100

    remove redundant line

--- public/app.js
+++ public/app.js
@@ -15,7 +15,7 @@ var app = function(){
     currencyView.createOption(currency)
     currencyView.createTargetOption(currency)
     currencyView.currencySelectElement.addEventListener("change",function(){
-      var newCurrencyCode = currencyView.otherCurrencies[this.value - 1]
+      // var newCurrencyCode = currencyView.otherCurrencies[this.value - 1]
       var newCurrency = new Currency()
       newCurrency.getData(function(currency){
         setup(currency)
